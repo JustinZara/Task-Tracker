2a3c2a2df5b5f5f46a85d654c21531a7ddac38a3
JustinZara <justin.zara41@qmail.cuny.edu>
2025-01-19 00:45:14 -0500

    made edits to updateTask function

--- tasktracker.cpp
+++ tasktracker.cpp
@@ -2,6 +2,7 @@
 #include <ctime>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 int numOfIDs = 0;
@@ -21,14 +22,33 @@ class task {
     string desc;
     string createdAt;
     string updatedAt;
+    string taskVariables;
 
     task(string newDesc, ofstream &file){//constructor for task object
+
         currID = numOfIDs++;
         createdAt = getCurrTime();
         updatedAt = getCurrTime();
         desc = newDesc;
         status = "todo";
-        string taskVariables = 
+        taskVariables = 
+        "{\n\t\"id\": \""         +to_string(currID)  +"\",\n"+
+        "\t\"description\": "   +desc               +",\n"+
+        "\t\"status\": \""        +status             +"\",\n"+
+        "\t\"createdAt\": \""     +createdAt          +"\",\n"+
+        "\t\"updatedAt\": \""     +updatedAt          +"\"\n"+
+        "}";
+
+        if(currID != 0)
+            file << ",\n";
+        file << taskVariables;//store variables in task file
+    }
+
+    void updateTask(string newDesc, ofstream &file, ifstream &inFile) {
+        desc = newDesc;
+        updatedAt = getCurrTime();
+        
+        string newTaskVariables = 
         "{\n\t\"id\": \""         +to_string(currID)  +"\",\n"+
         "\t\"description\": "   +desc               +",\n"+
         "\t\"status\": \""        +status             +"\",\n"+
@@ -43,7 +63,7 @@ class task {
 };
 
 bool strIsValidTask(string input) {//checks if user's inputted task is a valid string to store in the json file
-   bool isValid =  ((input.find("\"") == 0) && //first char is "
+   bool isValid =   ((input.find("\"") == 0) && //first char is "
                     (input.substr(input.length()-1) == "\"") && //char at last index is "
                     ((input.substr(1, input.length()-2).find("\"")) == std::string::npos));//chars in between arent "
     return isValid;
@@ -52,6 +72,9 @@ bool strIsValidTask(string input) {//checks if user's inputted task is a valid s
 int main() {
     string userInput;
     ofstream file("tasks.json");
+    ifstream inFile("tasks.json");
+    vector<task> tasks = {};
+    string newString;
 
     file << "[\n";
     while(userInput != "end") {
@@ -59,13 +82,20 @@ int main() {
         getline(cin, userInput);
         if(userInput.substr(0, 4) == "add " && strIsValidTask(userInput.substr(4))) {
             task newTask = task(userInput.substr(4), file);
+            tasks.push_back(newTask);
             cout << "Task added successfully (ID: "+to_string(newTask.currID)+")\n";
         } else if(userInput == "update") {
-
-        }
+            newString = userInput.substr(8);
+            cout << newString << " " << to_string(strIsValidTask(newString)) << "\n";
+            //tasks.at(stoi(userInput.substr(7, 1))).updateTask(newString, file, inFile);
+        } else if(userInput == "list")
+            for (int i = 0; i < tasks.size();i++) {
+                cout << tasks[i].desc << " ";
+            }
     }
-
     file << "\n]";
+    
     file.close();
+    inFile.close();
     return 0;
 }
