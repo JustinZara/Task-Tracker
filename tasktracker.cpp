6ace183984bc0965251e431e4dd8d672ec9aca4b
JustinZara <justin.zara41@qmail.cuny.edu>
2025-01-25 02:11:51 -0500

    FInished delete function

--- tasktracker.cpp
+++ tasktracker.cpp
@@ -30,15 +30,6 @@ class task {
         updatedAt = getCurrTime();
         desc = newDesc;
         status = "todo";
-        /*
-        taskVariables = 
-        "{\n\t\"id\": \""         +to_string(currID)  +"\",\n"+
-        "\t\"description\": "   +desc               +",\n"+
-        "\t\"status\": \""        +status             +"\",\n"+
-        "\t\"createdAt\": \""     +createdAt          +"\",\n"+
-        "\t\"updatedAt\": \""     +updatedAt          +"\"\n"+
-        "}";
-        */
         taskVariables = 
         "{\"id\": \""           +to_string(currID)  +"\","+
         "\"description\": "     +desc               +","+
@@ -68,53 +59,40 @@ class task {
     }
 };
 
-bool strIsValidTask(string input) {//checks if user's inputted task is a valid string to store in the json file
-   bool isValid =   ((input.find("\"") == 0) && //first char is "
-                    (input.substr(input.length()-1) == "\"") && //char at last index is "
-                    ((input.substr(1, input.length()-2).find("\"")) == std::string::npos));//chars in between arent "
+bool strIsValidTask(string input) {//checks if user's inputted task is a valid string to store in the json file (has quotes like "this")
+   bool isValid =   ((input.find("\"") == 0) && //valid if first char is "
+                    (input.substr(input.length()-1) == "\"") && //if char at last index is "
+                    ((input.substr(1, input.length()-2).find("\"")) == std::string::npos));// if chars in between arent "
     return isValid;
 }
 
 int main() {
     string userInput;
     ofstream file("tasks.json");
-    //ifstream inFile("tasks.json");
-    //string filename = "tasks.json";
-    vector<task> tasks = {};
+    vector<task> tasks;
+
     string newString;
     file << "[\n";
 
     while(userInput != "end") {
-        //cin >> userInput;
         getline(cin, userInput);
         if(userInput.substr(0, 4) == "add " && strIsValidTask(userInput.substr(4))) {
             task newTask = task(userInput.substr(4), file);
             tasks.push_back(newTask);
-            cout << "Task added successfully (ID: "+to_string(newTask.currID)+")\n";
-        } else if(userInput == "update") {
-            newString = userInput.substr(8);
-            cout << newString << " " << to_string(strIsValidTask(newString)) << "\n";
-            //tasks.at(stoi(userInput.substr(7, 1))).updateTask(newString, file, inFile);
+            cout << "Task added successfully (ID: "+to_string(newTask.currID)+")\n" << tasks.size() << " size\n";
+        } else if(userInput.substr(0, 7) == "update ") {
+            int index = (int)userInput.at(8);
+            //tasks.at(index).updateTask(newString, file, inFile);
         } else if(userInput == "list")
             for (int i = 0; i < tasks.size();i++) {
                 cout << tasks[i].desc << " ";
-            }
+        } else if(userInput.substr(0, 7) == "delete " && isdigit(userInput.at(7)) && (tasks.size() > 0)) {
+            int index = (int)userInput.at(7);
+            tasks.erase(tasks.begin()+index-1);
+        }
     }
     //ofstream file(filename);
     file << "\n]";
     file.close();
-
-    string lineInFile;
-    string lineToCompare = tasks[1].taskVariables;
-    int numOfLines = 0;
-    ifstream inFile("tasks.json");
-    while (getline(inFile, lineInFile)){
-        if(lineInFile == lineToCompare || lineInFile == lineToCompare+",") {
-            cout << "at line 0: " << lineToCompare << "\n";
-        }
-        cout << numOfLines++ << " ";
-        cout <<lineInFile << "\n";
-    }
-    inFile.close();
     return 0;
 }
